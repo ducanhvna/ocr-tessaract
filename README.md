52f75356184513104140852b539912cc9a4fc77c
Nguyễn Đức Anh <ducanhvna@outlook.com>
2020-04-24 15:44:47 +0700

    update funtion to calcualte IoU
    Signed-off-by: Nguyễn Đức Anh <ducanhvna@outlook.com>

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ocr-tessaract
  
+# Overview
+### Intersection over Union for object detection
+
+In the remainder of this blog post I’ll explain what the Intersection over Union evaluation metric is and why we use it.
+
+I’ll also provide a Python implementation of Intersection over Union that you can use when evaluating your own custom object detectors.
+
+Finally, we’ll look at some actual results of applying the Intersection over Union evaluation metric to a set of ground-truth and predicted bounding boxes.
+
+
+
+# references
+[!Intersection over Union (IoU) for object detection](https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/)
--- detectIoU.py
+++ detectIoU.py
@@ -38,3 +38,33 @@
 #      Cắt textblok
 #      Tính Độ chính xác của phương pháp
 ##########
+def bb_intersection_over_union(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
+
+
+# Load the meta format
+# Load file mata-format.json
+
+
+# Load Image
+# use tessaract detect image
+
+# B3 duyệt tất cả cac block trong meta format
+#           Tìm block nào có IoU cao nhất trong các block ảnh
+#            Vẽ block đó
