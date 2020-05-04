1b33c69e3216a82e40689a32760a6699c982dd10
Nguyễn Đức Anh <ducanhvna@outlook.com>
2020-05-04 17:04:44 +0700

    Update result for solution 2
    Signed-off-by: Nguyễn Đức Anh <ducanhvna@outlook.com>

--- detectIoU.py
+++ detectIoU.py
@@ -95,37 +95,12 @@ print('dy: ' + str(dy))
 n_boxes = len(d['level'])
 print ('level: ',n_boxes)
 ######### Solution 1: vùng đc chọn là vùng có iou to nhất
-for p in data['blocks']:
-    boxA = [p['left'], p['top'], p['left'] + p['width'], p['top'] + p['height'] ]
-
-    index = -1
-    maxvalue = 0
-
-    for i in range(n_boxes):
-        x0 = d['left'][i] / dx
-        y0 = d['top'][i] / dy
-        x1 = (d['left'][i] + d['width'][i]) / dx
-        y1 = (d['top'][i] + d['height'][i]) / dy
-
-        boxB = [x0, y0, x1, y1]
-        
-        iou = bb_intersection_over_union(boxA, boxB)
-        if iou > maxvalue:
-            index = i
-            maxvalue = iou
-    if index > -1 :
-        # print(d['level'][i])
-        (x, y, w, h) = (d['left'][index], d['top'][index], d['width'][index], d['height'][index])
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-##### Solution2 trong tất cả các vùng có iou >0 thì xét
-##### Buoc 1: Tim ra thanh phan nao to nhat
 # for p in data['blocks']:
 #     boxA = [p['left'], p['top'], p['left'] + p['width'], p['top'] + p['height'] ]
 
 #     index = -1
 #     maxvalue = 0
-#     avaiablelist = []
+
 #     for i in range(n_boxes):
 #         x0 = d['left'][i] / dx
 #         y0 = d['top'][i] / dy
@@ -135,36 +110,61 @@ for p in data['blocks']:
 #         boxB = [x0, y0, x1, y1]
         
 #         iou = bb_intersection_over_union(boxA, boxB)
-#         if iou >0: 
-#             avaiablelist.append(i)
 #         if iou > maxvalue:
-            
 #             index = i
 #             maxvalue = iou
 #     if index > -1 :
 #         # print(d['level'][i])
 #         (x, y, w, h) = (d['left'][index], d['top'][index], d['width'][index], d['height'][index])
-
-#         for i in avaiablelist:
-#             x0 = min(d['left'][i] / dx, x/dx)
-#             y0 = min(d['top'][i] / dy, y/dy)
-#             x1 = max((d['left'][i] + d['width'][i]) / dx, (x+w)/dx)
-#             y1 = max((d['top'][i] + d['height'][i]) / dy, (y+h)/dy)
-
-#             boxB = [x0, y0, x1, y1]
-        
-#             iou = bb_intersection_over_union(boxA, boxB)
-#             if iou > maxvalue:
-#                 x = int(x0)
-#                 y = int(y0)
-#                 w = int(x1 - x0)
-#                 h = int(y1 - y0)
-#                 maxvalue = iou    
-#         print(x)
-#         print(y)
-#         print(w)
-#         print(h)
 #         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+##### Solution2 trong tất cả các vùng có iou >0 thì xét
+##### Buoc 1: Tim ra thanh phan nao to nhat
+for p in data['blocks']:
+    boxA = [p['left'], p['top'], p['left'] + p['width'], p['top'] + p['height'] ]
+
+    index = -1
+    maxvalue = 0
+    avaiablelist = []
+    for i in range(n_boxes):
+        x0 = d['left'][i] / dx
+        y0 = d['top'][i] / dy
+        x1 = (d['left'][i] + d['width'][i]) / dx
+        y1 = (d['top'][i] + d['height'][i]) / dy
+
+        boxB = [x0, y0, x1, y1]
+        
+        iou = bb_intersection_over_union(boxA, boxB)
+        if iou >0: 
+            avaiablelist.append(i)
+        if iou > maxvalue:
+            
+            index = i
+            maxvalue = iou
+    if index > -1 :
+        # print(d['level'][i])
+        (x, y, w, h) = (d['left'][index], d['top'][index], d['width'][index], d['height'][index])
+
+        for i in avaiablelist:
+            x0 = min(d['left'][i] / dx, x/dx)
+            y0 = min(d['top'][i] / dy, y/dy)
+            x1 = max((d['left'][i] + d['width'][i]) / dx, (x+w)/dx)
+            y1 = max((d['top'][i] + d['height'][i]) / dy, (y+h)/dy)
+
+            boxB = [x0, y0, x1, y1]
+        
+            iou = bb_intersection_over_union(boxA, boxB)
+            if iou > maxvalue:
+                x = int(x0)
+                y = int(y0)
+                w = int(x1 - x0)
+                h = int(y1 - y0)
+                maxvalue = iou    
+        print(x)
+        print(y)
+        print(w)
+        print(h)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 #### Solution 3 Merge tung phia 1
 # for p in data['blocks']:
@@ -417,7 +417,7 @@ with open('data.txt', 'w') as outfile:
 cv2.waitKey(0)
 
 # Filename 
-filename = 'Images/savedImageSolution1.jpg'
+filename = 'Images/savedImageSolution2.jpg'
   
 # Using cv2.imwrite() method 
 # Saving the image 
