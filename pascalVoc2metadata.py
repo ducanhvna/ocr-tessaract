ea4618fba738288078c7ee04279c6bb3f219473f
Nguyễn Đức Anh <ducanhvna@outlook.com>
2020-05-04 15:26:49 +0700

    update src
    Signed-off-by: Nguyễn Đức Anh <ducanhvna@outlook.com>

---
+++ pascalVoc2metadata.py
@@ -0,0 +1,10 @@
+# Convert from xml pascal Voc format to meta data
+
+# Step by step
+# Step 1: Read xml file
+# Step 2: each block in xml file
+#           Add json object
+# Step3: Save json file
+
+# xml file: flow pascal Voc format
+# json file flow meta-data format
